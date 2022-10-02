db7453570d243e86c11721bc3392a32428f2e854
EladO0 <114872202+EladO0@users.noreply.github.com>
2022-10-02 19:56:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static object _MessageLock = new object();
+
+        public static void WriteMessage(string message, ConsoleColor c1)
+        {
+            lock (_MessageLock)
+            {
+                Console.BackgroundColor = c1;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+        }
+        static void Main(string[] args)
+        {
+            counter();
+            counter2();
+            Console.ReadLine();
+        }
+        public static async void counter()
+        {
+            await Task.Run(() =>
+            {
+                for(int i=0; i<100; i++)
+                {
+                    if(i % 10 == 0) { Console.Clear(); }
+                    Thread.Sleep(1000);
+                    WriteMessage("Worker#1 =>" + i,ConsoleColor.Green);
+                }
+            });
+        }
+        public static async void counter2()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i > -100; i--)
+                {
+                    if (i % 10 == 0) { Console.Clear(); }
+                    Thread.Sleep(1000);
+                    WriteMessage("Worker#2 =>" + i, ConsoleColor.Red);
+                }
+            });
+        }
+    }
+}
